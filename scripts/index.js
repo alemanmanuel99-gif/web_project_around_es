1139c92046b640574b70fda7863a411dc8e2f891
Manuel Aleman <alemanmanuel99@yahoo.com>
2026-05-09 15:37:03 -0600

    parte 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,60 @@
-let initialCards = [
-  {
-    name: "Valle de Yosemite",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
-  },
-  {
-    name: "Lago Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
-  },
-  {
-    name: "Montañas Calvas",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
-  },
-  {
-    name: "Parque Nacional de la Vanoise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
-  },
+/* --- 1. Array de Tarjetas Iniciales --- */
+const initialCards = [
+  { name: "Valle de Yosemite", link: "https://amazonaws.com" },
+  { name: "Lago Louise", link: "https://amazonaws.com" },
+  { name: "Montañas Calvas", link: "https://amazonaws.com" },
+  { name: "Latemar", link: "https://amazonaws.com" },
+  { name: "Parque Nacional de la Vanoise", link: "https://amazonaws.com" },
+  { name: "Lago di Braies", link: "https://amazonaws.com" },
 ];
 
-initialCards.forEach(function (card) {
+/* --- 2. Selección de Elementos del DOM (Almacenados en constantes) --- */
+const editPopup = document.querySelector("#edit-popup");
+const editButton = document.querySelector(".profile__edit-button");
+const closeButton = editPopup.querySelector(".popup__close");
+
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+const editForm = document.querySelector("#edit-profile-form");
+const nameInput = editForm.querySelector(".popup__input_type_name");
+const descriptionInput = editForm.querySelector(
+  ".popup__input_type_description",
+);
+
+/* --- 3. Funciones Generales de Modales --- */
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+/* --- 4. Funciones Específicas de Perfil --- */
+function fillProfileForm() {
+  nameInput.value = profileTitle.textContent;
+  descriptionInput.value = profileDescription.textContent;
+}
+
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal(editPopup);
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+  closeModal(editPopup);
+}
+
+/* --- 5. Detectores de Eventos --- */
+editButton.addEventListener("click", handleOpenEditModal);
+closeButton.addEventListener("click", () => closeModal(editPopup));
+editForm.addEventListener("submit", handleProfileFormSubmit);
+
+/* --- 6. Consola (Requisito de etapa anterior) --- */
+initialCards.forEach((card) => {
   console.log(card.name);
 });
